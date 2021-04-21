e185c1d1381ebbf756180bc379dd8ddf57cd6d59
adityamulik <aditya.mulik@gmail.com>
2021-04-21 06:05:16 +0530

    added github data & removed redundant files

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -20,36 +20,24 @@ class Portfolio extends Component {
   render() {
 
     if(this.state.githubData) {
-      console.log(this.state.githubData)
+      // console.log(this.state.githubData)
       var githubDataNew = this.state.githubData.map(function(item){
         return <div key={item.name}>
           <p>{item.name}</p>
         </div>
       })
-    }
-
-    if(this.props.data){
-      console.log(this.props.data.projects);
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+      console.log(githubDataNew)
+      for(let i=1; i<=githubDataNew.length; i++) {
+        var githubNo = () => {
+          return <div key={i}>
+            <p>{i}</p>
           </div>
-        </div>
-      })
+        }
+      }
     }
-
+    
     return (
+
       <section id="portfolio">
 
       <div className="row">
@@ -59,8 +47,16 @@ class Portfolio extends Component {
             <h1>Check Out Some of My Works.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
-                {githubDataNew}
+              <table>
+                <thead>
+                  <th>No</th>
+                  <th>Project Name</th>
+                </thead>
+                <tbody>
+                  <td>{githubNo}</td>
+                  <td>{githubDataNew}</td>
+                </tbody>
+              </table>
             </div>
           </div>
       </div>
