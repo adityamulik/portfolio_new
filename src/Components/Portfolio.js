5b77d167664a32ece240dfaee41e5ea8ffb0c7c6
adityamulik <aditya.mulik@gmail.com>
2021-04-25 10:59:26 +0530

    added react-carousel and github calendar

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Carousel from 'react-elastic-carousel';
+import Item from "./Item";
+import './Carousel.css';
+import GitHubCalendar from 'react-github-calendar';
+import ReactTooltip from 'react-tooltip';
 
 class Portfolio extends Component {
 
@@ -20,21 +25,21 @@ class Portfolio extends Component {
   render() {
 
     if(this.state.githubData) {
-      // console.log(this.state.githubData)
-      var githubDataNew = this.state.githubData.map(function(item){
-        return <div key={item.name}>
-          <p>{item.name}</p>
-        </div>
-      })
-      // console.log(githubDataNew)
-      for(let i=1; i<=githubDataNew.length; i++) {
-        var githubNo = () => {
-          return <div key={i}>
-            <p>{i}</p>
-          </div>
-        }
+      console.log(this.state.githubData[15]);
+      // const obj = this.state.githubData.map(data => {
+      //   return <Item key={data.name}>{data.name}</Item>
+      // })
+      for(let i=1; i<=this.state.githubData.length; i++) {
+        console.log(this.state.githubData[i]);
       }
     }
+
+    const breakPoints = [
+      {width: 1, itemsToShow: 1},
+      {width: 550, itemsToShow: 2},
+      {width: 768, itemsToShow: 3},
+      {width: 1200, itemsToShow: 4},
+    ]
     
     return (
 
@@ -44,10 +49,33 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Check Out Some of My Works on GitHub.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              <Carousel 
+                breakPoints={breakPoints}
+              >
+                <Item>
+                  <h2>GeoLocation API</h2>
+                </Item>
+                <Item>
+                  <h2>WebScrapping</h2>
+                </Item>
+                <Item>
+                  <h2>Task Management</h2>
+                </Item>
+                <Item>
+                  <h2>Relaxer App</h2>
+                </Item>
+              </Carousel>
+            </div>
+            <br></br>
+            <h1>My GitHub Calendar.</h1>
 
+            <div id="portfolio-wrapper" className="github-calendar" blockSize={14} blockMargin={4} fontSize={16}>
+              <GitHubCalendar username="adityamulik" color="hsl(203, 82%, 33%)" >
+                <ReactTooltip delayShow={50} html />
+              </GitHubCalendar>
             </div>
           </div>
       </div>
