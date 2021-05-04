47fad0bf0ea3a6d2ec84acd42b297170a4d24e23
adityamulik <aditya.mulik@gmail.com>
2021-05-04 05:13:22 -0400

    Email validation, submit button toggle for contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,21 +10,50 @@ class Contact extends Component {
          name: '',
          email: '',
          messageText: '',
+         nameValid: false,
+         emailValid: false,
+         messageValid: false,
          disabled: true
       }
+      this.handleNameChange = this.handleNameChange.bind(this);
+      this.handleEmailChange = this.handleEmailChange.bind(this);
+      this.handleMessageChange = this.handleMessageChange.bind(this);
    }
 
-   handleChange(e) {
-      if(e.target.name === "contactName") {
-         
-      }
-      else if (e.target.name === "contactEmail") {
-         
-      }
-      else if (e.target.name === "contactMessage") {
-         
-      }
+   handleNameChange(e) {
+      let nameValid = e.target.value ? true : false;
+      let submitValid = this.state.emailValid && this.state.messageValid && nameValid
+      this.setState({
+         name: e.target.value,
+         nameValid: nameValid,
+         disabled: !submitValid
+      });
    }
+
+   handleEmailChange(e) {
+      const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      let patternValid = pattern.test(e.target.value) ? true : false;
+      let emailValid = e.target.value ? true : false;
+      let submitValid = this.state.nameValid && this.state.messageValid && emailValid && patternValid
+      this.setState({
+         email: e.target.value,
+         emailValid: emailValid,
+         patternValid: patternValid,
+         disabled: !submitValid
+      });  
+   }
+
+
+   handleMessageChange(e) {
+      let messageValid = e.target.value ? true : false;
+      let submitValid = this.state.nameValid && this.state.emailValid && messageValid
+      this.setState({
+         messageText: e.target.value,
+         messageValid: messageValid,
+         disabled: !submitValid
+      });
+   }
+
 
    componentDidUpdate() {
 
@@ -70,17 +99,17 @@ class Contact extends Component {
 
                         <div>
                            <label htmlFor="contactName">Name <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                           <input type="text" defaultValue="" size="35" id="contactName" name="contactName" value={this.state.name} onChange={this.handleNameChange}/>
                         </div>
 
                         <div>
                            <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+                           <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" value={this.state.email} onChange={this.handleEmailChange}/>
                         </div>
 
                         <div>
                            <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                           <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" onChange={this.handleChange}>
+                           <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.messageText} onChange={this.handleMessageChange}>
                            </textarea>
                         </div>
 
