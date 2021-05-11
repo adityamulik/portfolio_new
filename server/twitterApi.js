63861e80f321163f321aa977bdb63cee3bbc2e45
adityamulik <aditya.mulik@gmail.com>
2021-05-11 13:58:16 -0400

    added created date for git api and limited tweets to 3 in contact component

--- server/twitterApi.js
+++ server/twitterApi.js
@@ -12,7 +12,8 @@ exports.handler = async (event, context, callback) => {
 
     const params = {
         'query': 'from:adityamulik',
-        'tweet.fields': 'author_id'
+        'tweet.fields': "attachments,author_id,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang",
+        'user.fields': 'username'
     }
 
     const res = await needle('get', endpointUrl, params, {
