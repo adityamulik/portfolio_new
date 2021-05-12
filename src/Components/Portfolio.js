4cbc999c46c59a15b65e415db8f073a3d8ce4c77
adityamulik <aditya.mulik@gmail.com>
2021-05-11 23:12:39 -0400

    removed all underlying errors and console messages

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Carousel from 'react-elastic-carousel';
-import Item from "./Item";
+// import Carousel from 'react-elastic-carousel';
+// import Item from "./Item";
 import './Carousel.css';
 import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
@@ -25,21 +25,21 @@ class Portfolio extends Component {
   render() {
 
     if(this.state.githubData) {
-      console.log(this.state.githubData[15]);
+      // console.log(this.state.githubData[15]);
       // const obj = this.state.githubData.map(data => {
       //   return <Item key={data.name}>{data.name}</Item>
       // })
       for(let i=1; i<=this.state.githubData.length; i++) {
-        console.log(this.state.githubData[i]);
+        // console.log(this.state.githubData[i]);
       }
     }
 
-    const breakPoints = [
-      {width: 1, itemsToShow: 1},
-      {width: 550, itemsToShow: 2},
-      {width: 768, itemsToShow: 3},
-      {width: 1200, itemsToShow: 4},
-    ]
+    // const breakPoints = [
+    //   {width: 1, itemsToShow: 1},
+    //   {width: 550, itemsToShow: 2},
+    //   {width: 768, itemsToShow: 3},
+    //   {width: 1200, itemsToShow: 4},
+    // ]
     
     return (
 
@@ -50,7 +50,7 @@ class Portfolio extends Component {
          <div className="twelve columns collapsed">
             <h1>My GitHub Calendar.</h1>
 
-            <div id="portfolio-wrapper" className="github-calendar" blockSize={14} blockMargin={4} fontSize={16}>
+            <div id="portfolio-wrapper" className="github-calendar">
               <GitHubCalendar 
                 username="adityamulik" 
                 color="#11ABB0" 
