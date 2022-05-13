41508a6cd9e95cf09319cac60261e7a0232e8917
adityamulik <aditya.mulik@gmail.com>
2022-05-13 17:25:02 -0400

    added co details and changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,20 +12,21 @@ class Footer extends Component {
     return (
       <footer>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
+        <div className="row">
+            <div className="twelve columns">
+              <ul className="social-links">
+                  {networks}
+              </ul>
 
-           <ul className="copyright">
-              <li>&copy; Copyright 2022 Aditya Mulik</li>
-           </ul>
+              <ul className="copyright">
+                  <li>&copy; Copyright 2022 Aditya Mulik</li>
+              </ul>
 
+            </div>
+            <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+        
+      </footer>
     );
   }
 }
